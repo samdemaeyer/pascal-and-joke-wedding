f8f3e5660b5dcce43ccde65cb043a814c18144c6
Sam De Maeyer <samdemaeyer@Sams-MacBook-Pro.local>
2024-04-11 12:03:58 +0100

    Format time units

--- components/Countdown/Countdown.tsx
+++ components/Countdown/Countdown.tsx
@@ -25,32 +25,34 @@ const getReturnValues = (countDown: number) => {
   return [days, hours, minutes, seconds];
 };
 
+const formatUnit = (number: number) => (number <= 9 ? `0${number}` : number);
+
 export const Countdown = () => {
-  const [days, hours, minutes, seconds] = useCountdown(new Date('2024-05-18 12:00:00'));
+  const [days, hours, minutes, seconds] = useCountdown(new Date('2024-05-18 13:00:00'));
 
   return (
     <div className="simply-countdown simply-countdown-one">
       <div className="simply-section simply-days-section">
         <div>
-          <span className="simply-amount">{days}</span>
+          <span className="simply-amount">{formatUnit(days)}</span>
           <span className="simply-word">days</span>
         </div>
       </div>
       <div className="simply-section simply-hours-section">
         <div>
-          <span className="simply-amount">{hours}</span>
+          <span className="simply-amount">{formatUnit(hours)}</span>
           <span className="simply-word">hours</span>
         </div>
       </div>
       <div className="simply-section simply-minutes-section">
         <div>
-          <span className="simply-amount">{minutes}</span>
+          <span className="simply-amount">{formatUnit(minutes)}</span>
           <span className="simply-word">minutes</span>
         </div>
       </div>
       <div className="simply-section simply-seconds-section">
         <div>
-          <span className="simply-amount">{seconds}</span>
+          <span className="simply-amount">{formatUnit(seconds)}</span>
           <span className="simply-word">seconds</span>
         </div>
       </div>
